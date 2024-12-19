0fe9a5433b60b3bafc03b85325d649fd987a102f
LornBunchuy <lorbunchuy@gmail.com>
2024-12-20 00:50:03 +0700

    Delete Django directory
    ok

--- Django/Mouse shop/shop/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from shop.models import Product
-from shop.models import Category
-
-admin.site.register(Product)
-admin.site.register(Category)
